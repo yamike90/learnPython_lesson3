0844e9821fee02b306391550ea56795621da8b8f
Mike Yampolsky <mike.yamp@gmail.com>
2019-09-21 13:43:42 +0300

    первое задание

---
+++ bus_stops_count.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('data-398-2019-09-18.csv', 'r', encoding='windows-1251') as bus_stops:
+    reader = csv.DictReader(bus_stops, delimiter=';')
+    streets_list = [] #создать пустой список с перечислением улиц из csv
+    for row in reader:
+        streets_list.append(row['Street']) #наполнить список значениями улиц
+    max_bus_stops_street = max(set(streets_list), key=streets_list.count) # преобразовать список во множество (юники), по каждому элементу множетсво получить количество повторений в списке с помощью функции count в key, сохранить в переменную улицы с наибольшем количеством повторений из key)
+print(max_bus_stops_street + ', ' + str(streets_list.count(max_bus_stops_street)))
